615a3349b26cef41ae9c65546337e69ac9b421c8
chparmley <63277749+chparmley@users.noreply.github.com>
2022-04-03 00:17:24 -0500

    Update flask_nginx_https.sh

--- flask_nginx_https.sh
+++ flask_nginx_https.sh
@@ -7,7 +7,9 @@ proj_name() {
 site_name() {
   read -p "Enter public IP address and/or DOMAINS to be used for the application seperated by spaces.\nExample: 165.156.51.125 mysite.com www.mysite.com: " domain_name
 }
-
+get_port() {
+  read -p "Enter desired application port: " application_port
+}
 setup_nginx() {
   sudo apt update
   sudo apt install nginx
@@ -25,8 +27,8 @@ setup_venv() {
 
 create_files() {
   touch ${project_name}.py
-  echo -e "from flask import Flask\napp=Flask(__name__)\n@app.route('/')\ndef home():\n  return 'Online!'\n\napp.run(host='0.0.0.0',port=5020)" > ${project_name}.py
-  sudo echo -e "\nserver {\n        root /var/www/html;\n\n        index index.html index.htm index.nginx-debian.html;\n    server_name $domain_name;\n\n        location /static {\n                alias $HOME/$proj_name/static;\n        }\n        location / {\n                try_files \$uri \$uri/ =404;\n                proxy_pass http://127.0.0.1:5020;\n                proxy_set_header Host \$host;\n                proxy_set_header X-Real-IP \$remote_addr;\n                proxy_redirect off;\n        }\n}" | sudo tee /etc/nginx/sites-available/default -a
+  echo -e "from flask import Flask\napp=Flask(__name__)\n@app.route('/')\ndef home():\n  return 'Online!'\n\napp.run(host='0.0.0.0',port=$application_port)" > ${project_name}.py
+  sudo echo -e "\nserver {\n        root /var/www/html;\n\n        index index.html index.htm index.nginx-debian.html;\n    server_name $domain_name;\n\n        location /static {\n                alias $HOME/$project_name/static;\n        }\n        location / {\n                try_files \$uri \$uri/ =404;\n                proxy_pass http://127.0.0.1:$application_port;\n                proxy_set_header Host \$host;\n                proxy_set_header X-Real-IP \$remote_addr;\n                proxy_redirect off;\n        }\n}" | sudo tee /etc/nginx/sites-available/default -a
 }
 
 config_certbot() {
@@ -42,6 +44,7 @@ load_application() {
 }
 
 proj_name;
+get_port;
 site_name;
 setup_nginx;
 setup_venv;
