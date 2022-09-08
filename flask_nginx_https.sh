835b8abbf07ca1fba758516450177cf4e6884aae
chparmley <63277749+chparmley@users.noreply.github.com>
2022-09-07 23:01:37 -0500

    changed install location to /var/www/

--- flask_nginx_https.sh
+++ flask_nginx_https.sh
@@ -12,7 +12,7 @@ get_port() {
 }
 setup_nginx() {
   sudo apt update
-  sudo aptt install -y python3 python3-pip nginx python3-setuptools git
+  sudo apt install -y python3 python3-pip nginx python3-setuptools git
   sudo apt install nginx
   sudo ufw allow 80
   sudo ufw allow 443
@@ -20,17 +20,17 @@ setup_nginx() {
 
 setup_venv() {
   sudo apt install python3-venv
-  mkdir $project_name && cd $project_name
+  sudo mkdir /var/www/${project_name}
   python3 -m venv venv
-  source venv/bin/activate
+  source /var/www/${project_name}/venv/bin/activate
   pip install uwsgi flask flask-restful gunicorn
   deactivate
 }
 
 create_files() {
-  touch ${project_name}.py
-  echo -e "from flask import Flask\napp=Flask(__name__)\n@app.route('/')\ndef home():\n  return 'Online!'\n\napp.run(host='0.0.0.0',port=$application_port)" > ${project_name}.py
-  sudo echo -e "\nserver {\n        root /var/www/html;\n\n        index index.html index.htm index.nginx-debian.html;\n    server_name $domain_name;\n\n        location /static {\n                alias $HOME/$project_name/static;\n        }\n        location / {\n                try_files \$uri \$uri/ =404;\n                proxy_pass http://127.0.0.1:$application_port;\n                proxy_set_header Host \$host;\n                proxy_set_header X-Real-IP \$remote_addr;\n                proxy_redirect off;\n        }\n}" | sudo tee /etc/nginx/sites-available/default -a
+  touch /var/www/${project_name}/${project_name}.py
+  echo -e "from flask import Flask\napp=Flask(__name__)\n@app.route('/')\ndef home():\n  return 'Online!'\n\napp.run(host='0.0.0.0',port=$application_port)" > /var/www/${project_name}.py
+  sudo echo -e "\nserver {\n        root /var/www/html;\n\n        index index.html index.htm index.nginx-debian.html;\n    server_name $domain_name;\n\n        location /static {\n                alias /var/www/${project_name}/static;\n        }\n        location / {\n                try_files \$uri \$uri/ =404;\n                proxy_pass http://127.0.0.1:$application_port;\n                proxy_set_header Host \$host;\n                proxy_set_header X-Real-IP \$remote_addr;\n                proxy_redirect off;\n        }\n}" | sudo tee /etc/nginx/sites-available/default -a
 }
 
 config_certbot() {
@@ -41,8 +41,8 @@ config_certbot() {
 
 load_application() {
   sudo systemctl restart nginx
-  source venv/bin/activate
-  python3 $project_name.py
+  source /var/www/${project_name}/venv/bin/activate
+  python3 /var/www/${project_name}/${project_name}.py
 }
 
 proj_name;
