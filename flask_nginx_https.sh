3c368790d9a89a51f472fdfcc80f2d7d384ebe51
chparmley <63277749+chparmley@users.noreply.github.com>
2022-09-07 23:14:14 -0500

    Update flask_nginx_https.sh

--- flask_nginx_https.sh
+++ flask_nginx_https.sh
@@ -28,8 +28,8 @@ setup_venv() {
 }
 
 create_files() {
-  touch /var/www/${project_name}/${project_name}.py
-  echo -e "from flask import Flask\napp=Flask(__name__)\n@app.route('/')\ndef home():\n  return 'Online!'\n\napp.run(host='0.0.0.0',port=$application_port)" > /var/www/${project_name}.py
+  sudo touch /var/www/${project_name}/${project_name}.py
+  sudo echo -e "from flask import Flask\napp=Flask(__name__)\n@app.route('/')\ndef home():\n  return 'Online!'\n\napp.run(host='0.0.0.0',port=$application_port)" > /var/www/${project_name}.py
   sudo echo -e "\nserver {\n        root /var/www/html;\n\n        index index.html index.htm index.nginx-debian.html;\n    server_name $domain_name;\n\n        location /static {\n                alias /var/www/${project_name}/static;\n        }\n        location / {\n                try_files \$uri \$uri/ =404;\n                proxy_pass http://127.0.0.1:$application_port;\n                proxy_set_header Host \$host;\n                proxy_set_header X-Real-IP \$remote_addr;\n                proxy_redirect off;\n        }\n}" | sudo tee /etc/nginx/sites-available/default -a
 }
 
